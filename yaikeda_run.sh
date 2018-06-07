e6ef64556f3a8fa7ca1c52ee621b794c8cd52abd
Yasunari Ikeda <ikutakaiesiak@gmail.com>
2018-06-07 12:23:54 +0900

    Add script file to run my own program.

---
+++ yaikeda_run.sh
@@ -0,0 +1,1 @@
+python src/openpose_3dpose_sandbox.py --camera_frame --residual --batch_norm --dropout 0.5 --max_norm --evaluateActionWise --use_sh --epochs 200 --load 4874200 --openpose data_openpose/other/onegai_durling/json --output_filename other_onegai_durling --output_dirname output/other/onegai_durling --write_output_img True
